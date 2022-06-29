8f8b9e3d3ecd02d1cbd8f1a92219feacc4438413
HHHHHyuk <keealsgur@gmail.com>
2022-06-29 18:31:45 +0900

    5장 내용 커밋 ( 2/2 )
    Naver 계정 + Test 메소드들 오류 제거

--- src/main/java/com/awsstudy/springboot/web/IndexController.java
+++ src/main/java/com/awsstudy/springboot/web/IndexController.java
@@ -1,9 +1,11 @@
 package com.awsstudy.springboot.web;
 
+import com.awsstudy.springboot.config.auth.LoginUser;
 import com.awsstudy.springboot.config.auth.dto.SessionUser;
 import com.awsstudy.springboot.service.posts.PostsService;
 import com.awsstudy.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.h2.engine.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,9 +21,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if(user!=null){
             model.addAttribute("userName", user.getName());
         }
